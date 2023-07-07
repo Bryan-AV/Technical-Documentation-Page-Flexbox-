9fa6ee9c573242ef7b8a3c91b02e949f84e62646
Bryan Aguilar <bav29cr@gmail.com>
2023-07-07 15:36:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$(".navbar-nav>li>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  $(".navbar-nav>li>ul>li").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  
+
